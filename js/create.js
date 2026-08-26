296b9c36f03fc2326f6f04eb55512cf19e4eca85
rwaindok <r.waindok@outlook.de>
2026-08-26 17:02:59 +0200

    create.js e_button

---
+++ js/create.js
@@ -0,0 +1,9 @@
+function e_button(text, onc=""){
+	let butt = "";
+    butt += "<button id='b" + id + "'";
+    id++;
+    if(onc!=""){
+    	butt += " onclick='" + onc + "'";
+    }
+    butt += ">" + text + "</button>"
+}
